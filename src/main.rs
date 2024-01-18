77150a8703d860c00974b4161e303874954d1eb9
Matthias Geier <Matthias.Geier@gmail.com>
2024-01-18 20:40:58 +0100

    Simplify impls

--- src/main.rs
+++ src/main.rs
@@ -63,23 +63,15 @@ trait AddImpl {
 
 type Add<L, R> = <(L, R) as AddImpl>::Output;
 
-impl AddImpl for (Zero, Zero) {
-    type Output = Zero;
+impl<T> AddImpl for (Zero, T) {
+    type Output = T;
 }
 
-impl<T> AddImpl for (Succ<T>, Zero) {
-    type Output = Succ<T>;
-}
-
-impl<T> AddImpl for (Zero, Succ<T>) {
-    type Output = Succ<T>;
-}
-
-impl<T, U> AddImpl for (Succ<T>, Succ<U>)
+impl<T, U> AddImpl for (Succ<T>, U)
 where
-    (T, Succ<Succ<U>>): AddImpl,
+    (T, Succ<U>): AddImpl,
 {
-    type Output = Add<T, Succ<Succ<U>>>;
+    type Output = Add<T, Succ<U>>;
 }
 
 // Subtraction
@@ -90,12 +82,8 @@ trait SubImpl {
 
 type Sub<L, R> = <(L, R) as SubImpl>::Output;
 
-impl SubImpl for (Zero, Zero) {
-    type Output = Zero;
-}
-
-impl<T> SubImpl for (Succ<T>, Zero) {
-    type Output = Succ<T>;
+impl<T> SubImpl for (T, Zero) {
+    type Output = T;
 }
 
 impl<T> SubImpl for (Zero, Succ<T>) {
@@ -117,23 +105,17 @@ trait MulImpl {
 
 type Mul<L, R> = <(L, R) as MulImpl>::Output;
 
-impl<T> MulImpl for (T, Zero) {
+impl<T> MulImpl for (Zero, T) {
     type Output = Zero;
 }
 
-// Implementing for (Zero, T) would overlap with the previous impl, so we do
-// (Zero, Succ<T>) to avoid the (Zero, Zero) case that overlaps
-impl<T> MulImpl for (Zero, Succ<T>) {
-    type Output = Zero;
-}
-
-impl<T, U> MulImpl for (Succ<T>, Succ<U>)
+impl<T, U> MulImpl for (Succ<T>, U)
 where
-    (T, Succ<U>): MulImpl,
-    (Succ<U>, Mul<T, Succ<U>>): AddImpl,
+    (T, U): MulImpl,
+    (U, Mul<T, U>): AddImpl,
 {
     // x * y = y + (x - 1) * y
-    type Output = Add<Succ<U>, Mul<T, Succ<U>>>;
+    type Output = Add<U, Mul<T, U>>;
 }
 
 // Division
@@ -144,15 +126,11 @@ trait GreaterThanEqImpl {
 
 type GreaterThanEq<L, R> = <(L, R) as GreaterThanEqImpl>::Output;
 
-impl GreaterThanEqImpl for (Zero, Zero) {
-    type Output = Succ<Zero>;
-}
-
 impl<T> GreaterThanEqImpl for (Zero, Succ<T>) {
     type Output = Zero;
 }
 
-impl<T> GreaterThanEqImpl for (Succ<T>, Zero) {
+impl<T> GreaterThanEqImpl for (T, Zero) {
     type Output = Succ<Zero>;
 }
 
@@ -169,32 +147,26 @@ trait DivImpl {
 
 type Div<L, R> = <(L, R) as DivImpl>::Output;
 
-// Instead of implementing for (T, Succ<Zero>), implement for (Succ<T>, Succ<Zero>)
-// to avoid overlapping with the next impl on (Zero, Succ<Zero>)
-impl<T> DivImpl for (Succ<T>, Succ<Zero>) {
-    type Output = Succ<T>;
-}
-
-impl<T> DivImpl for (Zero, T) {
+impl<T> DivImpl for (Zero, Succ<T>) {
     type Output = Zero;
 }
 
-type RawQuotient<T, U> = Add<Succ<Zero>, Div<Sub<Succ<T>, Succ<Succ<U>>>, Succ<Succ<U>>>>;
+type RawQuotient<T, U> = Add<Succ<Zero>, Div<Sub<Succ<T>, Succ<U>>, Succ<U>>>;
 
-impl<T, U> DivImpl for (Succ<T>, Succ<Succ<U>>)
+impl<T, U> DivImpl for (Succ<T>, Succ<U>)
 where
-    (T, Succ<U>): SubImpl,
-    (Sub<T, Succ<U>>, Succ<Succ<U>>): DivImpl,
-    (Succ<Zero>, Div<Sub<T, Succ<U>>, Succ<Succ<U>>>): AddImpl,
+    (T, U): SubImpl,
+    (Sub<T, U>, Succ<U>): DivImpl,
+    (Succ<Zero>, Div<Sub<T, U>, Succ<U>>): AddImpl,
     (
-        GreaterThanEq<Succ<T>, Succ<Succ<U>>>,
-        Add<Succ<Zero>, Div<Sub<T, Succ<U>>, Succ<Succ<U>>>>,
+        GreaterThanEq<T, U>,
+        Add<Succ<Zero>, Div<Sub<T, U>, Succ<U>>>,
     ): MulImpl,
-    (Succ<T>, Succ<Succ<U>>): GreaterThanEqImpl,
+    (T, U): GreaterThanEqImpl,
 {
     // If x < y, return 0. We can do this by multiplying the "RawQuotient" by
     // the bool->int value of this condition.
-    type Output = Mul<GreaterThanEq<Succ<T>, Succ<Succ<U>>>, RawQuotient<T, U>>;
+    type Output = Mul<GreaterThanEq<Succ<T>, Succ<U>>, RawQuotient<T, U>>;
 }
 
 #[cfg(test)]
